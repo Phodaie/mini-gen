fde48bc16a7b558b258fd0fe0acf05d00832de0f
payman hodaie <payman@osellus-mobile.com>
2023-08-16 11:59:11 -0400

    refactored functions

---
+++ functions/functions.py
@@ -0,0 +1,124 @@
+import os
+from duckduckgo_search import DDGS
+
+
+x = 10
+
+def show_x():
+    print(x)
+
+
+project_dir = "./generated"
+
+def show_project_dir():
+    print(project_dir)
+
+def set_project_dir(dir:str):
+    project_dir = dir
+    create_directory_if_not_exists(project_dir)
+
+def debug_args(*args_to_print):
+    GREEN = '\033[92m'  # Green color
+    YELLOW = '\033[93m'  # Yellow color
+    RESET = '\033[0m'   # Reset color to default
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            arg_names = func.__code__.co_varnames[:func.__code__.co_argcount]
+            print(f"{GREEN}Function name: {func.__name__}{RESET}")
+            
+            # Only show arguments specified in args_to_print
+            for arg_name, value in zip(arg_names, args):
+                if arg_name in args_to_print:
+                    print(f"{YELLOW}{arg_name}: {value}{RESET}")
+            for key, value in kwargs.items():
+                if key in args_to_print:
+                    print(f"{YELLOW}{key}: {value}{RESET}")
+
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+#@debug_args("keywords")
+def web_text_search(keywords: str):
+        max_results = 10
+        results = []
+        for r in DDGS().text(keywords=keywords):
+            results.append(r)
+            if (max_results and len(results) >= max_results):
+                break
+        return results
+
+def get_clarifiction(requiredClarification:str)->str:
+    clarification = input(requiredClarification)
+    return clarification
+
+
+
+
+def create_directory_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Directory {path} created.")
+    else:
+        print(f"Directory {path} already exists.")
+
+#@debug_args()
+def get_file_paths() -> list[str]:
+    """
+    Return a list of file paths in the current directory.
+
+    Returns:
+    - List[str]: The list of file paths.
+    """
+    import os
+    file_paths = []
+    for file_name in os.listdir():
+        if os.path.isfile(file_name):
+            file_paths.append(file_name)
+    return file_paths
+
+def list_files_in_directory():
+    """
+    Returns a list of files in the specified directory.
+    
+    Returns:
+    - list: List of files in the directory.
+    """
+    with os.scandir(project_dir) as entries:
+        return [entry.name for entry in entries if entry.is_file()]
+
+
+#@debug_args("filename")
+def read_file_content(filename: str) -> str:
+    """
+    Read the content of a file with the given filename.
+
+    Args:
+    - filename (str): The name of the file to be read.
+
+    Returns:
+    - str: The content of the file.
+    """
+    file_path = os.path.join(project_dir, filename)
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
+#@debug_args("filename")
+def write_to_file(filename: str, content: str) -> None:
+    """
+    Create a file with the given filename and write the provided content to it.
+
+    Args:
+    - filename (str): The name of the file to be created.
+    - content (str): The content to be written to the file.
+
+    Returns:
+    - None
+    """
+    #project_dir = "./generated"
+    file_path = os.path.join(project_dir, filename)
+
+    with open(file_path, 'w') as file:
+        file.write(content)
--- main.py
+++ main.py
@@ -1,148 +1,32 @@
+
 import os
-from duckduckgo_search import DDGS
-#from marvin import openai
-from marvin.core.ChatCompletion.providers.openai import ChatCompletion as OpenAIChatCompletion
-from marvin.core.ChatCompletion.base import BaseConversationState
-
-class FConversationState(BaseConversationState):
-     def send(self, *args, **kwargs):
-        if self.turns:
-            kwargs["messages"] = [
-                *self.turns[-1].request.messages,
-                self.turns[-1].raw.choices[0].message.to_dict(),
-                *kwargs.get("messages", []),
-            ]
-
-        response = self.model.create(*args, **kwargs)
-        self.turns.append(response)
-        return response
+from marvin import openai
+import functions.functions
+from functions.functions import  get_clarifiction , write_to_file , web_text_search , read_file_content , list_files_in_directory , create_directory_if_not_exists
 
 
+GREEN = '\033[92m'  # Green color
+YELLOW = '\033[93m'  # Yellow color
+RESET = '\033[0m'   # Reset color to default
 
-class FChatCompletion(OpenAIChatCompletion):
-    
-    _state_class: FConversationState
-
-    def state(self, *args, **kwargs):
-        return FConversationState(self, *args, **kwargs)
-     
-    def prepare_request(self, **kwargs):
-        return self.request(
-            **(self._defaults or {}), **self.dict(exclude={"_defaults"})
-        ).merge(**kwargs)
-
-    def create(self, *args, **kwargs):
-        request = self.prepare_request(**kwargs)
-        request_dict = request.schema()
-        create = getattr(self.model(request), self._create)
-        return self.response(raw=create(*args, **request_dict), request=request)
-
-ChatCompletion = FChatCompletion()
-
-def web_text_search(keywords: str):
-        max_results = 10
-        results = []
-        for r in DDGS().text(keywords=keywords):
-            results.append(r)
-            if (max_results and len(results) >= max_results):
-                break
-        return results
-
-def get_clarifiction(requiredClarification:str)->str:
-    clarification = input(requiredClarification)
-    return clarification
-
-ROOT_DIR = "./generated"
-
-def create_directory_if_not_exists(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-        print(f"Directory {path} created.")
-    else:
-        print(f"Directory {path} already exists.")
-
-def get_file_paths() -> list[str]:
-    """
-    Return a list of file paths in the current directory.
-
-    Returns:
-    - List[str]: The list of file paths.
-    """
-    import os
-    file_paths = []
-    for file_name in os.listdir():
-        if os.path.isfile(file_name):
-            file_paths.append(file_name)
-    return file_paths
-
-def list_files_in_directory():
-    """
-    Returns a list of files in the specified directory.
-    
-    Returns:
-    - list: List of files in the directory.
-    """
-    with os.scandir(ROOT_DIR) as entries:
-        return [entry.name for entry in entries if entry.is_file()]
-
-
-
-def read_file_content(filename: str) -> str:
-    """
-    Read the content of a file with the given filename.
-
-    Args:
-    - filename (str): The name of the file to be read.
-
-    Returns:
-    - str: The content of the file.
-    """
-    file_path = os.path.join(ROOT_DIR, filename)
-    with open(file_path, 'r') as file:
-        content = file.read()
-    return content
-
-
-def write_to_file(filename: str, content: str) -> None:
-    """
-    Create a file with the given filename and write the provided content to it.
-
-    Args:
-    - filename (str): The name of the file to be created.
-    - content (str): The content to be written to the file.
-
-    Returns:
-    - None
-    """
-    #ROOT_DIR = "./generated"
-    file_path = os.path.join(ROOT_DIR, filename)
-
-    with open(file_path, 'w') as file:
-        file.write(content)
-
-available_funcs = {get_clarifiction , write_to_file , web_text_search , read_file_content , list_files_in_directory }
-used_funcs = set()
-
-project_dir = input("What directory do you want to use?\n")
-if len(project_dir) > 0:
-    ROOT_DIR = project_dir
-
-create_directory_if_not_exists(ROOT_DIR)
+dir = input("What directory do you want to use?\n")
+if len(dir) > 0:
+    functions.functions.project_dir = dir
 
 user_prompt = input("What do you want to do?\n")
 
-with ChatCompletion(functions=list(available_funcs),model="gpt-4" ) as conversation:
+used_funcs = set()
+available_funcs = {get_clarifiction , write_to_file , web_text_search , read_file_content , list_files_in_directory }
+with openai.ChatCompletion(functions=list(available_funcs),model="gpt-4" ) as conversation:
 
     system_prompt = f'''
     You are an expert iOS developer. 
-    Create a xCode project that includes all the requrired source code files, resorece files etc.
-    Create .xcodeproj and info.plist so the project can be opened in Xcode.
+    Create all the requrired source code files.
     Escape Special Characters: Ensure that all special characters within the Swift code, such as double quotes (") and backslashes (\), are properly escaped. For example, a double quote should be represented as \" and a backslash as \\ in the JSON string.
     '''
     user_prompt = f'''
     {user_prompt} 
     Prompt the user if you need any clarification.
-    First get the latest ios development documentation from the web before generating the project.
     Escape Special Characters: Ensure that all special characters within the Swift code, such as double quotes (") and backslashes (\), are properly escaped. For example, a double quote should be represented as \" and a backslash as \\ in the JSON string.
    
     '''
@@ -154,15 +38,27 @@ with ChatCompletion(functions=list(available_funcs),model="gpt-4" ) as conversat
         ],
         temperature=0.0,
     )
+    
+    while True:
+        while conversation.last_response.has_function_call():
+            response = conversation.last_response
+            print(f"{GREEN}Function name: {response.choices[0].message.function_call.name}{RESET}")
+            
+            used_funcs.add(response.choices[0].message.function_call.name)
+            conversation.send(messages=[response.call_function()])
+            
 
-    while conversation.last_response.has_function_call():
-        response = conversation.last_response
-        print(response.choices[0].message.function_call)
-        used_funcs.add(response.choices[0].message.function_call.name)
-        conversation.send(messages=[response.call_function()])
-        
+        print(conversation.last_response.choices[0].message.content)
 
-    print(conversation.last_response.choices[0].message.content)
+        next_prompt = input("Next step (type end to exit)\n")
+        if next_prompt == "end":
+            break
+
+        conversation.send(messages=[
+            {"role": "user", "content": next_prompt},
+        ],
+        temperature=0.0,
+        )
 
     available_func_names = {func.__name__ for func in available_funcs}
     print(f"Used functions: {used_funcs}")
