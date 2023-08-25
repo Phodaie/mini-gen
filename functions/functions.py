8417c937bac66a1d5ec54751add4a7e4a4c4a9aa
payman hodaie <payman@osellus-mobile.com>
2023-08-25 00:14:34 -0400

    added build and methaphor search functions

--- functions/functions.py
+++ functions/functions.py
@@ -1,11 +1,12 @@
 import os
+import subprocess
 from duckduckgo_search import DDGS
+from metaphor_python import Metaphor
 
 
-x = 10
-
-def show_x():
-    print(x)
+GREEN = '\033[92m'  
+YELLOW = '\033[93m' 
+RESET = '\033[0m' 
 
 
 project_dir = "./generated"
@@ -33,21 +34,27 @@ def debug_args(*args_to_print):
         return wrapper
     return decorator
 
-#@debug_args("keywords")
-def web_text_search(keywords: str):
-        max_results = 10
-        results = []
-        for r in DDGS().text(keywords=keywords):
-            results.append(r)
-            if (max_results and len(results) >= max_results):
-                break
-        return results
+
+
+def web_text_search(query: str)->str:
+    print(f"{GREEN}Serarching: {query}{RESET}")
+    metaphor = Metaphor(os.getenv("METAPHOR_API_KEY"))
+    search_response = metaphor.search(query,num_results=5,include_domains=["developer.apple.com"])
+    contents_response = search_response.get_contents()
+
+    output = ""
+    for content in contents_response.contents:
+        output += (content.extract + "\n")
+
+    return output
+
+
+
 
 def get_clarifiction(requiredClarification:str)->str:
+    print(f"{GREEN}Asking for clarification{RESET}")
     clarification = input(requiredClarification)
     return clarification
-
-
 
 
 def create_directory_if_not_exists(path):
@@ -57,7 +64,34 @@ def create_directory_if_not_exists(path):
     else:
         print(f"Directory {path} already exists.")
 
-#@debug_args()
+def list_all_files_relative() -> list[str]:
+    """
+    List all files in the given root directory, including those in subdirectories, 
+    with paths relative to the root directory. Ignores files and directories with a . prefix.
+
+    Args:
+    - root_dir (str): The root directory to start the search from.
+
+    Returns:
+    - list: A list of file paths relative to the root directory.
+    """
+    root_dir = project_dir
+    file_list = []
+
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        # Remove directories with . prefix from the list of directories to be explored
+        dirnames[:] = [d for d in dirnames if not d.startswith('.')]
+        
+        for filename in filenames:
+            if not filename.startswith('.'):
+                relative_path = os.path.relpath(os.path.join(dirpath, filename), root_dir)
+                file_list.append(relative_path)
+
+    return file_list
+
+print(list_all_files_relative())
+
+
 def get_file_paths() -> list[str]:
     """
     Return a list of file paths in the current directory.
@@ -65,6 +99,7 @@ def get_file_paths() -> list[str]:
     Returns:
     - List[str]: The list of file paths.
     """
+    print(f"{GREEN}Getting files{RESET}")
     import os
     file_paths = []
     for file_name in os.listdir():
@@ -79,6 +114,7 @@ def list_files_in_directory():
     Returns:
     - list: List of files in the directory.
     """
+    print(f"{GREEN}list of files{RESET}")
     with os.scandir(project_dir) as entries:
         return [entry.name for entry in entries if entry.is_file()]
 
@@ -94,6 +130,7 @@ def read_file_content(filename: str) -> str:
     Returns:
     - str: The content of the file.
     """
+    print(f"{GREEN}Reading: {filename}{RESET}")
     file_path = os.path.join(project_dir, filename)
     with open(file_path, 'r') as file:
         content = file.read()
@@ -111,8 +148,58 @@ def write_to_file(filename: str, content: str) -> None:
     Returns:
     - None
     """
-    #project_dir = "./generated"
+    print(f"{GREEN}Writing: {filename}{RESET}")
     file_path = os.path.join(project_dir, filename)
 
     with open(file_path, 'w') as file:
         file.write(content)
+
+
+
+
+
+
+def run_xcodebuild(scheme_name: str, configuration: str, sdk: str, project_path: str = None, workspace_path: str = None) -> str:
+    """
+    Execute the xcodebuild command with the given parameters and return its output.
+
+    Args:
+    - scheme_name (str): The name of the scheme to build.
+    - configuration (str): The build configuration (e.g., "Release" or "Debug").
+    - sdk (str): The SDK to build against (e.g., "iphoneos").
+    - project_path (str, optional): The path to the .xcodeproj file.
+    - workspace_path (str, optional): The path to the .xcworkspace file.
+
+    Returns:
+    - str: The output of the xcodebuild command.
+    """
+    
+    print(f"{GREEN}Building {RESET}")
+    
+    project_path = "/Users/payman/Develop/WorldTime/WorldTime.xcodeproj"
+    scheme =  "WorldTime"
+    cmd = ["xcodebuild", "-quiet"]
+    
+    if project_path:
+        cmd.extend(["-project", project_path])
+    elif workspace_path:
+        cmd.extend(["-workspace", workspace_path])
+    
+    cmd.extend([
+        "-scheme", scheme_name,
+        "-configuration", configuration,
+        "-sdk", sdk,
+        "build"
+    ])
+
+    try:
+        # Capture the output and return it
+        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
+        return result
+    except subprocess.CalledProcessError as e:
+        # If there's an error, capture the error output and return it
+        return e.output
+
+# output = run_xcodebuild("WorldTime", "Develop", "iphoneos", project_path="/path/to/YourProject.xcodeproj")
+# print(output)
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from marvin import openai
 import functions.functions
-from functions.functions import  get_clarifiction , write_to_file , web_text_search , read_file_content , list_files_in_directory 
+from functions.functions import  get_clarifiction , write_to_file , web_text_search , read_file_content , list_files_in_directory , run_xcodebuild , list_all_files_relative
 
 
 GREEN = '\033[92m'  
@@ -14,18 +14,21 @@ if len(dir) > 0:
 
 user_prompt = input("What do you want to do?\n")
 
-available_funcs = {get_clarifiction , write_to_file , web_text_search , read_file_content , list_files_in_directory }
+available_funcs = {get_clarifiction , write_to_file , web_text_search , read_file_content , list_all_files_relative , run_xcodebuild}
 with openai.ChatCompletion(functions=list(available_funcs),model="gpt-4" ) as conversation:
 
     system_prompt = f'''
     You are an expert iOS developer. 
     Create all the requrired source code files.
+    You are a Large Languge Model whose trainning was till Sep 2021. It's Aug 2023 so make sure you search the web for the latest development documentation.
     Escape Special Characters: Ensure that all special characters within the Swift code, such as double quotes (") and backslashes (\), are properly escaped. For example, a double quote should be represented as \" and a backslash as \\ in the JSON string.
     '''
     user_prompt = f'''
     {user_prompt} 
     Prompt the user if you need any clarification.
+    After changing source code, build the project and fix bugs if any.
     Escape Special Characters: Ensure that all special characters within the Swift code, such as double quotes (") and backslashes (\), are properly escaped. For example, a double quote should be represented as \" and a backslash as \\ in the JSON string.
+    
     '''
     
     conversation.send(messages=[
@@ -38,7 +41,7 @@ with openai.ChatCompletion(functions=list(available_funcs),model="gpt-4" ) as co
     while True:
         while conversation.last_response.has_function_call():
             response = conversation.last_response
-            print(f"{GREEN}Function name: {response.choices[0].message.function_call.name}{RESET}")
+            # print(f"{GREEN}Function name: {response.choices[0].message.function_call.name}{RESET}")
             conversation.send(messages=[response.call_function()])
             
         print(conversation.last_response.choices[0].message.content)
